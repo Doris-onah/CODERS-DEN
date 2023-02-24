b87a0b0c9d9ebe686b7b85e7bad36beeae1410e6
Doris <onahdorisene@gmail.com>
2023-02-24 08:22:08 +0000

    day2

--- Day2.js
+++ Day2.js
@@ -81,3 +81,54 @@ console.log(repeatedStr); // output: '30 Days Of JavaScript30 Days Of JavaScript
 
 
 
+//EXCERICES LEVEL 2
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+
+let num ="10";
+
+let Python = "Python jargon";
+let includes= Python.includes("on");
+console.log(includes);
+
+let jargon = "I hope this course is not full of jargon";
+let checkIncludes = jargon.includes("jargon");
+console.log(checkIncludes);
+
+let random = Math.floor(Math.random() * 101);
+console.log(random);
+
+let random1 = Math.floor(Math.random() * 51) + 50;
+console.log(random1);
+
+let random3 = Math.floor(Math.random() * 255 - 0) + 0;
+console.log(random3);
+
+
+const strg = "JavaScript";
+const randChar = strg.charAt(Math.floor(Math.random() * strg.length));
+console.log(randChar);
+
+console.log("1\t1\t1\t1\t1");
+console.log("2\t1\t2\t4\t8");
+console.log("3\t1\t3\t9\t27");
+console.log("4\t1\t4\t16\t64");
+console.log("5\t1\t5\t25\t125");
+
+
+let sent = "You cannot end a sentence with because because because is a conjunction";
+let SubSent = sent.substr(31, 23);
+console.log(SubSent);
+
+//LEVEL 3
+let love = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+let numTimes = love.match(/love/gi).length;
+console.log(numTimes);
+
+let findMatch = 'You cannot end a sentence with because because because is a conjunction';
+let findMatch2 = findMatch.match(/because/gi);
+
+const sentenceClean = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and&';
+const cleanedSentence = sentenceClean.replace(/[%$@&#+;]/g, '').toLowerCase();
+console.log(cleanedSentence);
